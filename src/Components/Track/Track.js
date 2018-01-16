daf4bc634524abef970687aab5e11f520c1714ec
Twan Kruiswijk <twankrui@gmail.com>
2018-01-16 11:34:10 +0100

    Implemented Feature request preview audio

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -9,7 +9,6 @@ class Track extends Component {
     this.removeTrack = this.removeTrack.bind(this);
   }
 
-
   renderAction() {
     const {onAdd} = this.props;
 
@@ -32,15 +31,32 @@ class Track extends Component {
     onRemove(track);
   }
 
+  renderPreview() {
+    const {track: {previewURL}} = this.props;
+
+    if (previewURL === null) {
+      return <p className="previewMessage">There is no preview available</p>
+    }
+    
+    return (
+      <audio controls preload="none">
+        <source src={previewURL} type="audio/mpeg"/>
+      </audio>
+    );
+  }
+
   render() {
     const {track: {name, artist, album}} = this.props;
     const trackAction = this.renderAction();
+    const audioPreview = this.renderPreview();
 
     return (
       <div className="Track">
         <div className="Track-information">
           <h3>{name}</h3>
           <p>{artist} | {album}</p>
+
+          {audioPreview}
         </div>
 
         {trackAction}
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -44,6 +44,7 @@ const Spotify = {
               artist: track.artists[0].name,
               album: track.album.name,
               uri: track.uri,
+              previewURL: track.preview_url,
             }
           });
         });
